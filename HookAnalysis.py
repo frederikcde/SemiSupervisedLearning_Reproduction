5dba966dcc4af9a4f25606943265faea692d9153
frederikcde <frederik@cdehg.nl>
2021-04-11 23:48:35 +0200

    Added convergence computation together with CPU time - output table

--- HookAnalysis.py
+++ HookAnalysis.py
@@ -33,7 +33,9 @@ NEURAL NETWORK CODE ORIGINAL REPOSITORY:
 
 # Import Statements
 import os
+import math as m
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 plt.close("all")
@@ -62,12 +64,18 @@ benchmark_sample_size = 50000
 benchmark_file_name = hook_dir + "hook_" + str(benchmark_sample_size) + ".txt"
 benchmark_hook = np.genfromtxt(benchmark_file_name)
 
+# Make dictionary with run specifics
+# Sample size : [labels, hours]
+algorithm_evaluation_specifics = { 10000 : [600, 22.300],
+                                   20000 : [1200, 14.017],
+                                   50000 : [3000, 48.000]}
+
 # Set font size for plots
 font_size = 16
 plt.rcParams.update({'font.size': font_size})
 
 ########################################################################
-# PERFORM ANALSYIS #####################################################
+# PERFORM ACCURACY ANALSYIS ############################################
 ########################################################################
 
 ############### Initialize figures ###############
@@ -141,4 +149,51 @@ error_figure.savefig(output_dir + "error_figure")
 
 delta_figure.tight_layout()
 delta_figure.savefig(output_dir + "delta_figure")
+
+########################################################################
+# PERFORM CONVERGENCE ANALSYIS #########################################
+########################################################################
+
+# Define convergence requirement
+convergence_requirement = 0.001
+
+# Loop over hook files
+for current_evaluation in algorithm_evaluation_specifics.keys():
+    # Obtain file
+    current_file_name = hook_dir + "hook_" + str(current_evaluation) + ".txt"
+    current_hook = np.genfromtxt(current_file_name)
+    # Compute cpu time
+    current_specifics = algorithm_evaluation_specifics[ current_evaluation ]
+    total_CPU_time = current_specifics[1] * 3600
+    # Obtain test errors
+    current_test_error = current_hook[:, 4]
     
+    # Initialize storage list for converged epochs
+    converged_epochs = []
+    
+    # Loop over test error values
+    for error_index in range(110, len(current_test_error)):
+        # Compute current average error over last 10 epochs
+        current_avg_error = np.mean(current_test_error[(error_index-10):error_index])
+        # Compute average error over 10 epochs 100 epochs back
+        previous_avg_error = np.mean(current_test_error[(error_index-110):(error_index-100)])
+        # Compute convergence level
+        convergence_level = (previous_avg_error - current_avg_error)/current_avg_error
+        # Store converged epoch if true
+        if convergence_level < convergence_requirement:
+            converged_epochs.append(error_index)
+    
+    # Obtain output settings
+    first_converged_epoch = min(converged_epochs)
+    accuracy = current_test_error[first_converged_epoch]
+    converged_CPU_time = first_converged_epoch/len(current_test_error) * total_CPU_time
+    converged_CPU_time_h = round(converged_CPU_time/3600,3)
+    
+    # Update dictionary
+    current_specifics.append(first_converged_epoch) 
+    current_specifics.append(converged_CPU_time_h)
+    current_specifics.append(accuracy)
+    algorithm_evaluation_specifics[ current_evaluation ] = current_specifics
+
+output_file_columns = ["N", "Total $T_{CPU}$ [h]", "Converged Epoch", "Converged $T_{CPU}$ [h]", "Accuracy"]
+output_table = pd.DataFrame(data=algorithm_evaluation_specifics.values(), columns=output_file_columns, index=algorithm_evaluation_specifics.keys())
